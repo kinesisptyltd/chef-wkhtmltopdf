22719956c7ba57c01c3a62b2c85d991eab241107
Orel Sokolov <orelcokolov@gmail.com>
2016-02-26 15:15:55 +0300

    Update wkhtmltopdf recipe

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,4 @@
-default['wkhtmltopdf']['version']     = '0.12.0'
-default['wkhtmltopdf']['build_sha']   = '03c001d'
+default['wkhtmltopdf']['version']     = '0.12.3'
 default['wkhtmltopdf']['install_dir'] = '/usr/local/bin'
 default['wkhtmltopdf']['lib_dir']     = ''
 
@@ -20,11 +19,11 @@ else
     %w(fedora rhel) => %w(fontconfig libXext libXrender openssl-devel urw-fonts)
   )
   if node['kernel']['machine'] == 'x86_64'
-    default['wkhtmltopdf']['platform'] = 'linux-amd64'
+    default['wkhtmltopdf']['platform'] = 'linux-generic-amd64'
   else
-    default['wkhtmltopdf']['platform'] = 'linux-i386'
+    default['wkhtmltopdf']['platform'] = 'linux-generic-i386'
   end
 end
 
-default['wkhtmltopdf']['archive']     = "wkhtmltox-#{node['wkhtmltopdf']['platform']}_#{node['wkhtmltopdf']['version']}-#{node['wkhtmltopdf']['build_sha']}.tar.xz"
-default['wkhtmltopdf']['mirror_url']  = "http://downloads.sourceforge.net/project/wkhtmltopdf/#{node['wkhtmltopdf']['version']}/#{node['wkhtmltopdf']['archive']}"
+default['wkhtmltopdf']['archive']     = "wkhtmltox-#{node['wkhtmltopdf']['version']}_#{node['wkhtmltopdf']['platform']}.tar.xz"
+default['wkhtmltopdf']['mirror_url']  = "http://download.gna.org/wkhtmltopdf/0.12/#{node['wkhtmltopdf']['version']}/#{node['wkhtmltopdf']['archive']}"
